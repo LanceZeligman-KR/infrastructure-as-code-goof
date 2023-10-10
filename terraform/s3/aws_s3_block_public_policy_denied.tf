aa68899983dbfe90ff32b015f2a33fde112e39d2
Lance Zeligman <lance.zeligman@kroger.com>
2023-10-10 16:19:21 -0400

    no public policy

--- terraform/s3/aws_s3_block_public_policy_denied.tf
+++ terraform/s3/aws_s3_block_public_policy_denied.tf
@@ -1,6 +1,3 @@
 resource "aws_s3_bucket_public_access_block" "denied" {
-  block_public_policy = false
-}
-
-resource "aws_s3_bucket_public_access_block" "denied_2" {
+  block_public_policy = true
 }
