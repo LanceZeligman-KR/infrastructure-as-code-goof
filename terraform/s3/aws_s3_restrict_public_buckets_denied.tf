6ad38fae586a750bada71b7b99e27eeefe59a6fa
Lance Zeligman <lance.zeligman@kroger.com>
2023-10-10 16:17:45 -0400

    no public buckets

--- terraform/s3/aws_s3_restrict_public_buckets_denied.tf
+++ terraform/s3/aws_s3_restrict_public_buckets_denied.tf
@@ -1,7 +1,3 @@
 resource "aws_s3_bucket_public_access_block" "denied" {
   block_public_policy = true
 }
-
-resource "aws_s3_bucket_public_access_block" "denied_2" {
-  restrict_public_buckets = true
-}
